74568ffc37846ebbffbc882dfe1a3810f297f1b2
Imam Nurhy <imamnurhy@gmail.com>
2023-07-27 00:36:13 +0700

    Update

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Welcome to Notify API').status(200);
+    res.send('Selamat data di Telegram Api').status(200);
 });
 
 router.use('/message', require('./message'))

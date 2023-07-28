7e369f0656329556c4d425a1c067d0f2e245b923
Imam Nurhy <imamnurhy@gmail.com>
2023-07-29 02:36:31 +0700

    Change template message

--- src/controller/messageController.js
+++ src/controller/messageController.js
@@ -7,65 +7,40 @@ const MessageController = {
         const { message, code, title, detail, datetime } = req.body;
 
 
-        // var text = '';
-        // text = `<b>${title.toUpperCase()}`;
-        // text += ' | ';
-        // text += httpStatus.getReasonPhrase(code) + '</b>\n\n';
+        var text = '';
+        text = `<b>${title.toUpperCase()}`;
+        text += ' | ';
+        text += httpStatus.getReasonPhrase(code) + '</b>\n\n';
 
-        // // if (code) {
-        // //     var emoticon = '❤️';
-        // //     if (code >= 200 && code <= 226) emoticon = '🟢';
-        // //     if (code >= 400 && code <= 511) emoticon = '❗';
-        // //     text += `Status ${code + ' ' + emoticon} \n`;
-        // // }
-
-        // // text += `${lineSeparator}\n`;
-
-        // if (detail) {
-        //     text += `${detail}\n`;
-        // } else {
-        //     text += `${message}\n`;
-        // }
-
-        // text += '\nKet:\n';
-
-        // if (datetime) {
-        //     text += '•Tanggal ' + dayjs(datetime).format('DD MMMM YYYY') + '\n';
-        //     text += '•Waktu ' + dayjs(datetime).format('HH:mm:ss') + '\n';
-
-        //     var emoticon = '';
+        // if (code) {
+        //     var emoticon = '❤️';
+        //     if (code >= 200 && code <= 226) emoticon = '🟢';
         //     if (code >= 400 && code <= 511) emoticon = '❗';
-        //     text += '•Status ' + `${code}` + `${emoticon}`;
+        //     text += `Status ${code + ' ' + emoticon} \n`;
         // }
 
-        const titleText = `**${title.toUpperCase()} | ${httpStatus.getReasonPhrase(code)}**`;
+        // text += `${lineSeparator}\n`;
 
-        const messageText = `${message}`;
-
-        let detailText = '';
         if (detail) {
-            detailText = '**Detail**';
-            detailText += `\`\`\`json
-            ${detail}
-            \`\`\``;
+            text += `<pre><code class="language-json">${detail}</code></pre>\n`;
+        } else {
+            text += `${message}\n`;
         }
 
-        let descText = '**Keterangan**';
+        text += '\nKet:\n';
 
         if (datetime) {
-            descText += `* Tanggal ${dayjs(datetime).format('DD MMMM YYYY')}`;
-            descText += `* Waktu ${dayjs(datetime).format('HH:mm:ss')}`;
+            text += '•Tanggal ' + dayjs(datetime).format('DD MMMM YYYY') + '\n';
+            text += '•Waktu ' + dayjs(datetime).format('HH:mm:ss') + '\n';
+
+            var emoticon = '';
+            if (code >= 400 && code <= 511) emoticon = '❗';
+            text += '•Status ' + `${code}` + `${emoticon}`;
         }
-
-        let emoticon = '';
-        if (code >= 400 && code <= 511) emoticon = '❗';
-        descText += `* Status ${code}${emoticon}`;
-
-        const errorText = `${titleText}\n\n${messageText}\n\n${detailText}\n${descText}`;
 
         try {
             const chatId = req.chatId;
-            await bot.sendMessage(chatId, errorText, { parse_mode: 'Markdown' });
+            await bot.sendMessage(chatId, messageText, { parse_mode: 'HTML' });
             return res.status(200).json({
                 status: 0,
                 message: 'Pesan terkirim',

dea2805ef2e8f6bea3e30eb2c3781b2be993c285
Imam Nurhy <imamnurhy@gmail.com>
2023-07-31 20:53:51 +0700

    Update

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send('Selamat data di Tebot Api').status(200);
+    res.send('Selamat data di Tebot Apis').status(200);
 });
 
 app.use('/api', router);

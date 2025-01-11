566b0fd86b9d30b91992db1ca009cac2181db8e1
Imam Nurhy <imamnurhy@gmail.com>
2025-01-11 11:15:07 +0700

    Fix - bug encode chatId

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -1,10 +1,10 @@
 const encodeWithSecretKey = (id) => {
-    const buffer = Buffer.from(id, 'utf-8');
+    const buffer = Buffer.from(String(id), 'utf-8');
     return buffer.toString('base64');
 }
 
 const decodeWithSecretKey = (encryptedID) => {
-    const buffer = Buffer.from(encryptedID, 'base64');
+    const buffer = Buffer.from(String(encryptedID), 'base64');
     return buffer.toString('utf-8');
 };
 
